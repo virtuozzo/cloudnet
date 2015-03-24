7b7951ebce0f7a9e113afcb00bb5a34c1e29d020
Tomasz Widuch <tomasz@onapp.com>
2015-03-24 14:56:16 +0100

    common layout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < Devise::SessionsController
+  layout "public"
+  
   def new
     flash[:notice] = session.delete(:registration_flash) if session[:registration_flash]
     super
