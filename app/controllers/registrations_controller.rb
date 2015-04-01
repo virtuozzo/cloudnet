8758433249a44092152a5cb32eab7cbbb6076e2f
Tomasz Widuch <tomasz@onapp.com>
2015-04-01 11:25:45 +0100

    order summary on login screen

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,7 @@
 class RegistrationsController < Devise::RegistrationsController
   layout "public"
+  include SessionOrderReport
+  before_action :prepare_order
   # def new
   # end
   #
@@ -10,7 +12,7 @@ class RegistrationsController < Devise::RegistrationsController
 
   def new
     # We want to have the ability to fill in some params
-    prepare_order if session[:user_return_to]
+    #prepare_order if session[:user_return_to]
     build_resource(sign_up_params)
     respond_with resource
   end
@@ -22,20 +24,4 @@ class RegistrationsController < Devise::RegistrationsController
     super
   end
   
-  private
-  
-  def prepare_order
-    @server_params =  Rack::Utils.parse_nested_query(
-                         URI::parse(session[:user_return_to]).query).symbolize_keys
-    @location = Location.find(@server_params[:id])
-    set_price
-  end
-  
-  def set_price
-    @h_price = @location.hourly_price( @server_params[:mem].to_i,
-                                     @server_params[:cpu].to_i,
-                                     @server_params[:disk].to_i).to_f
-    @m_price = (@h_price * 672 / 1000).round.to_f / 100
-    @h_price = @h_price.round.to_f / 100000
-  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,7 @@
 class SessionsController < Devise::SessionsController
   layout "public"
+  include SessionOrderReport
+  before_action :prepare_order
   
   def new
     flash[:notice] = session.delete(:registration_flash) if session[:registration_flash]
