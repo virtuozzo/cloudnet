c87826fc14015490de45a5f9a013d9e0b81c90b7
Thomas Buckley-Houston <tom@tombh.co.uk>
2015-04-15 16:21:41 +0100

    Re-raise exceptions in dev/test when errors are caught by Sentry

--- lib/error_logging/sentry_logging.rb
+++ lib/error_logging/sentry_logging.rb
@@ -2,10 +2,11 @@ require 'raven'
 require 'raven/integrations/sidekiq'
 
 class SentryLogging < ErrorLogging::Methods
+  CAPTURABLE_ENVS = %w(production staging)
   def initialize
     Raven.configure do |config|
-      config.environments = %w(production staging)
-      config.dsn = LOGGING[:sentry][:dsn] if Rails.env.in? %w(production staging)
+      config.environments = CAPTURABLE_ENVS
+      config.dsn = LOGGING[:sentry][:dsn] if Rails.env.in? CAPTURABLE_ENVS
       config.silence_ready = true
     end
   end
@@ -13,6 +14,8 @@ class SentryLogging < ErrorLogging::Methods
   def track_exception(e, params)
     Rails.logger.warn "Exception Message: #{e.message}"
     Rails.logger.warn "Backtrace:\n\t#{e.backtrace.join("\n\t")}"
+
+    raise e unless Rails.env.in? CAPTURABLE_ENVS
 
     Raven.capture_exception(e, params)
   end
