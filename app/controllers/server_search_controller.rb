9da63bc9a249a1105c6feb014e8c14ed3472a756
Tomasz Widuch <twiduch@gmail.com>
2015-05-21 14:51:54 +0100

    segment test

--- app/controllers/server_search_controller.rb
+++ app/controllers/server_search_controller.rb
@@ -2,6 +2,9 @@ class ServerSearchController < ApplicationController
   skip_before_action :authenticate_user!
   #before_action {redirect_to :root if user_signed_in?}
   layout "public"
+  def index
+    Analytics.track(current_user, event: 'Marketplace - online') if current_user
+  end
   
   def create
     redirect_to :sign_in
