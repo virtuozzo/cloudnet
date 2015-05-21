b4fd03293e82982734b26cd6c6b343e0bfdd6bd7
Tomasz Widuch <twiduch@gmail.com>
2015-05-21 15:45:10 +0100

    analytics for offline user

--- app/controllers/server_search_controller.rb
+++ app/controllers/server_search_controller.rb
@@ -2,11 +2,18 @@ class ServerSearchController < ApplicationController
   skip_before_action :authenticate_user!
   #before_action {redirect_to :root if user_signed_in?}
   layout "public"
+  
   def index
-    Analytics.track(current_user, event: 'Marketplace - online') if current_user
+    analytics_info
   end
   
   def create
     redirect_to :sign_in
+  end
+  
+  private
+  def analytics_info
+    event = current_user ? "online" : "offline"
+    Analytics.track(current_user, event: 'Marketplace - ' + event)
   end
 end
