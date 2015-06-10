01e01e633fc3cbb9348b6638197633d35194a6fe
Tomasz Widuch <twiduch@gmail.com>
2015-06-10 13:30:48 +0100

    images for activeadmin

--- app/admin/certificate.rb
+++ app/admin/certificate.rb
@@ -3,6 +3,43 @@ ActiveAdmin.register Certificate do
   permit_params :name, :avatar, :description
   
 
+  index do
+    column :id
+    column :name
+    column :description
+    column "Avatar" do |cert|
+      image_tag(cert.avatar)
+    end
+    
+    actions
+  end
+  
+  show do
+    attributes_table do
+      row :id
+      row :name
+      row :description
+      row :created_at
+      row :updated_at
+      row :avatar
+      row "Avatar" do |cert|
+        image_tag(cert.avatar)
+      end
+    end
+  end
+  
+  form do |f|
+    semantic_errors *object.errors.keys
+
+    inputs do
+      input :name
+      input :description
+      input :avatar, as: :file, :hint => image_tag(object.avatar.url)
+      input :avatar_cache, :as => :hidden
+    end
+
+    actions
+  end
   
   sidebar :control_panel_links do
     ul do
