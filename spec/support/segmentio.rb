56dbbf877f6bf65bbb502c561f7e54455e229baf
Tomasz Widuch <twiduch@gmail.com>
2015-07-16 14:00:53 +0100

    tests update

--- spec/support/segmentio.rb
+++ spec/support/segmentio.rb
@@ -1,6 +1,7 @@
 RSpec.configure do |config|
   config.before(:each) do
-    allow(Segment::Analytics).to receive(:new).and_return(double('Segment::Analytics', identify: true, track: true))
+    analytics_double = double('Segment::Analytics', identify: true, track: true, alias: true, flush: true)
+    allow(Segment::Analytics).to receive(:new).and_return(analytics_double)
   end
 
   # rspec-rails 3 will no longer automatically infer an example group's spec type
