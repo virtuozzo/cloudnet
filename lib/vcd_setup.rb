13cebe51a6b444f20f1ad4390190dc4470f39329
Thomas Buckley-Houston <tom@tombh.co.uk>
2015-08-12 09:44:57 +0100

    Rubocop warnings fixe in vcd_setup.rb

--- lib/vcd_setup.rb
+++ lib/vcd_setup.rb
@@ -1,15 +1,15 @@
+# Get vCD env setup
 class VCDSetup
   class << self
     def api
       OnappBlanketAPI.new.admin_connection
     end
-    
+
     def import_templates
-      html = open("#{ENV['ONAPP_CP']}/vapps/new",
-        {
-          http_basic_authentication: ['cloudnetvcd@onapp', ENV['VCDEMO_PASS']],
-          ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE
-        }
+      html = open(
+        "#{ENV['ONAPP_CP']}/vapps/new",
+        http_basic_authentication: ['cloudnetvcd@onapp', ENV['VCDEMO_PASS']],
+        ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE
       )
       doc = Nokogiri::HTML html
       options = doc.css '#vapp_vapp_template_id option'
@@ -27,7 +27,7 @@ class VCDSetup
         # )
       end
     end
-    
+
     def get_vmid(id)
       details = api.get(
         "/vapp_templates/#{id}/hardware_customization",
