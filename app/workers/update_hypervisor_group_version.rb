8794dcd08a7175d3dfe34db5a7a57e667455b4bb
Tinu Cleatus <tinupc@gmail.com>
2015-10-05 15:57:36 +0100

    Made exception safe

--- app/workers/update_hypervisor_group_version.rb
+++ app/workers/update_hypervisor_group_version.rb
@@ -6,9 +6,13 @@ class UpdateHypervisorGroupVersion
     squall = Squall::HypervisorZone.new(uri: ONAPP_CP[:uri], user: ONAPP_CP[:user], pass: ONAPP_CP[:pass])
     hv_zones = squall.list
     hv_zones.each do |hv_zone|
-      location = Location.where(hv_group_id: hv_zone["id"]).first
-      next if location.nil?
-      location.update_attribute(:hv_group_version, hv_zone["supplier_version"])
+      begin
+        location = Location.where(hv_group_id: hv_zone["id"]).first
+        next if location.nil?
+        location.update_attribute(:hv_group_version, hv_zone["supplier_version"])
+      rescue Exception => e
+        ErrorLogging.new.track_exception(e, extra: { source: 'UpdateHypervisorGroupVersion', hv_zone: hv_zone["id"] })
+      end
     end
   end
 end
