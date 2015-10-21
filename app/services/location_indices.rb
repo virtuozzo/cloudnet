edf8a00859375afb70f05545a1a7c9ed34833eb2
Tomasz Widuch <twiduch@gmail.com>
2015-10-21 14:55:36 +0200

    remove indices with 0

--- app/services/location_indices.rb
+++ app/services/location_indices.rb
@@ -12,7 +12,13 @@ class LocationIndices
   
     def indices
       stat = Squall::Statistic.new(*squall_params)
-      stat.cloud_score(federation_id).map(&:symbolize_keys)
+      stat.cloud_score(federation_id).map do |index|
+        next if index.any? {|k,v| v.is_a?(Fixnum) and v == 0 }
+        index.symbolize_keys
+      end.compact
+    rescue Faraday::Error::ClientError => e
+      log_error(e)
+      []
     end
   
     def federation_id
@@ -26,5 +32,16 @@ class LocationIndices
   
     def squall_params
       [uri: ONAPP_CP[:uri], user: ONAPP_CP[:user], pass: ONAPP_CP[:pass]]
+    end
+    
+    def log_error(e)
+      ErrorLogging.new.track_exception(
+        e,
+        extra: {
+          current_location: @location,
+          source: 'LocationIndices',
+          faraday: e.response
+        }
+      )
     end
 end
