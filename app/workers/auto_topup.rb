9f481bb0476c5250d1ca6e85828798a24819b20e
Tinu Cleatus <tinupc@gmail.com>
2016-02-03 00:15:57 +0530

    Auto top up Wallet for users - WIP

---
+++ app/workers/auto_topup.rb
@@ -0,0 +1,36 @@
+# Automatically top-up the Wallet of a user with the minimum valid top-up amount as per Payg::VALID_TOP_UP_AMOUNTS
+# Conditions to be met for the top-up to be made:
+#       * Have atleast one active servers
+#       * Have atleast one processable billing card
+#       * Have a Wallet balance of less than $2
+
+class AutoTopup
+  include Sidekiq::Worker
+  sidekiq_options unique: true
+
+  def perform
+    Account.find_each do |account|
+      user = account.user
+      puts "================================================="
+      puts "Account ID: #{account.id}"
+      puts "User: #{user.email}"
+      puts "Servers: #{user.servers.count}"
+      puts "Cards: #{account.billing_cards.processable.count}"
+      puts "Wallet balance: #{account.wallet_balance}"
+      next unless user.servers.count > 0 && 
+                  account.billing_cards.processable.count > 0 && 
+                  account.wallet_balance < 200_000
+      begin
+        puts "Topping up..."
+        task = PaygTopupCardTask.new(account, Payg::VALID_TOP_UP_AMOUNTS.min)
+        if task.process
+          puts "Done!"
+          unpaid_invoices = account.invoices.not_paid
+          ChargeInvoicesTask.new(user, unpaid_invoices).process unless unpaid_invoices.empty?
+        end
+      rescue Exception => e
+        ErrorLogging.new.track_exception(e, extra: { current_user: user, source: 'AutoTopup' })
+      end
+    end
+  end
+end
---
+++ spec/workers/auto_topup_spec.rb
@@ -0,0 +1,71 @@
+require 'rails_helper'
+
+describe AutoTopup do
+  
+  before :each do
+    @user = FactoryGirl.create :user
+    @account = @user.account
+    @payments = double(Payments, auth_charge: { charge_id: 12_345 }, capture_charge: { charge_id: 12_345 })
+    allow(Payments).to receive_messages(new: @payments)
+  end
+  
+  after :each do
+    SidekiqUniqueJobs.redis_mock.flushdb
+  end
+  
+  it 'should topup Wallet' do
+    credit_note = FactoryGirl.create :credit_note, account: @account
+    FactoryGirl.create :credit_note_item, credit_note: credit_note, net_cost: 100_000
+    FactoryGirl.create :server, user: @user
+    FactoryGirl.create :billing_card, account: @account, fraud_verified: true
+    expect(@account.wallet_balance).to eq(100_000)
+    
+    AutoTopup.perform_async
+    assert_equal 1, AutoTopup.jobs.size
+    
+    AutoTopup.drain
+    @account.reload
+    expect(@account.wallet_balance).to eq((Payg::VALID_TOP_UP_AMOUNTS.min * Invoice::MILLICENTS_IN_DOLLAR).to_i + 100_000)
+  end
+  
+  it 'shoul not topup Wallet because there are no active servers' do
+    credit_note = FactoryGirl.create :credit_note, account: @account
+    FactoryGirl.create :credit_note_item, credit_note: credit_note, net_cost: 100_000
+    FactoryGirl.create :billing_card, account: @account, fraud_verified: true
+    expect(@account.wallet_balance).to eq(100_000)
+    
+    AutoTopup.perform_async
+    assert_equal 1, AutoTopup.jobs.size
+    
+    AutoTopup.drain
+    @account.reload
+    expect(@account.wallet_balance).to eq(100_000)
+  end
+  
+  it 'shoul not topup Wallet because there are no processable cards' do
+    FactoryGirl.create :billing_card, account: @account, fraud_verified: false
+          
+    AutoTopup.perform_async
+    assert_equal 1, AutoTopup.jobs.size
+    
+    AutoTopup.drain
+    @account.reload
+    expect(@account.wallet_balance).to eq(0)
+  end
+  
+  it 'should not topup Wallet because there are enough Wallet balance' do
+    credit_note = FactoryGirl.create :credit_note, account: @account
+    FactoryGirl.create :credit_note_item, credit_note: credit_note, net_cost: 500_000
+    FactoryGirl.create :server, user: @user
+    FactoryGirl.create :billing_card, account: @account, fraud_verified: true
+    expect(@account.wallet_balance).to eq(500_000)
+    
+    AutoTopup.perform_async
+    assert_equal 1, AutoTopup.jobs.size
+    
+    AutoTopup.drain
+    @account.reload
+    expect(@account.wallet_balance).to eq(500_000)
+  end
+  
+end
