e52dbc2d48055f6181ef5db332aeb17c1d5fbcfe
Tomasz Widuch <twiduch@gmail.com>
2016-02-08 19:25:31 +0100

    refactoring docker provision monitor

--- app/workers/docker_provision.rb
+++ app/workers/docker_provision.rb
@@ -2,7 +2,7 @@ class DockerProvision
   include Sidekiq::Worker
   sidekiq_options unique: true
   sidekiq_options :retry => 5
-  attr_reader :server_id, :role, :job_id
+  attr_reader :server_id, :server, :role, :job_id
   
   FINAL_STATUS = %w(Failed Done Error Aborted)
   POLLING_TIME = 30.seconds
@@ -11,6 +11,7 @@ class DockerProvision
     
   def perform(server_id, role)
     @server_id = server_id
+    @server = Server.find(server_id)
     @role = role
     @job_id = nil
     
@@ -18,14 +19,17 @@ class DockerProvision
     case resp.status
     when 200..209
       @job_id = resp.body
-      status = wait_for_server_provisioned(job_id)
-      finalize_build(status[:status])
+      status = wait_for_server_provisioned(job_id)[:status]
+      raise(ProvisionerError, status) unless status == "Done"
     else 
       raise(ProvisionerError, resp.status)
     end
-    
   rescue => e
+    Rails.logger.warn "Provisioner of #{role} for server #{server_id} failed"
     ErrorLogging.new.track_exception(e, extra: prov_error_params)
+  ensure
+    unset_server_from_provision
+    ServerTasks.new.perform(:refresh_server, server.user.id, server_id)
   end
   
   def send_data_to_provisioner
@@ -41,25 +45,13 @@ class DockerProvision
     end
     status
   end
-  
-  def finalize_build(status)
-    raise(ProvisionerError, status) unless status == "Done"
-  rescue => e
-    Rails.logger.warn "Provisioner of #{role} for server #{server_id} failed"
-    ErrorLogging.new.track_exception(e, extra: prov_error_params)
-  ensure
-    server = Server.find(server_id)
-    unset_server_from_provision(server)
-    user_id = server.user.id
-    ServerTasks.new.perform(:refresh_server, user_id, server_id)
-  end
-  
+ 
   def job_status(job_id)
     resp = provision_tasks.status(job_id)
     resp.status == 200 ? JSON.parse(resp.body).symbolize_keys : {status: "Error"}
   end
   
-  def unset_server_from_provision(server)
+  def unset_server_from_provision
     server.update_attribute(:in_provision, false)
   end
   
