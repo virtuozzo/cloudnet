d7311255d5fcf97b4d1b27421aa070ceb344489e
Tomasz Widuch <twiduch@gmail.com>
2016-11-24 12:06:08 +0000

    refactor module methods

---
+++ lib/build_checker/build_checker.rb
@@ -0,0 +1,16 @@
+# Builds test VMs using all accessible templates
+module BuildChecker
+  PID_KEY = 'build_checker_pid'
+
+  def self.running?
+    System.get(PID_KEY).present?
+  end
+
+  def self.running!
+    System.set(PID_KEY, Process.pid)
+  end
+
+  def self.pid
+    System.get(PID_KEY).to_i
+  end
+end
--- lib/build_checker/orchestrator.rb
+++ lib/build_checker/orchestrator.rb
@@ -1,19 +1,5 @@
-# Builds test VMs using all accessible templates
 module BuildChecker
-  PID_KEY = 'build_checker_pid'
-
-  def self.running?
-    System.get(PID_KEY).present?
-  end
-
-  def self.running!
-    System.set(PID_KEY, Process.pid)
-  end
-
-  def self.pid
-    System.get(PID_KEY).to_i
-  end
-
+  # start and stop all build checker services
   class Orchestrator
     include BuildChecker::Logger
     @@threads = []
